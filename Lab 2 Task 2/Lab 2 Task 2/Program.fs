0fa6b9053d6eb17b9d7afade37ad1d4f189a7774
Kriperus <krasnyhaleksandr123@gmail.com>
2026-03-06 02:47:22 +0500

    Add files via upload

---
+++ Lab 2 Task 2/Lab 2 Task 2/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open Microsoft.FSharp.Core
+
+let rec createList () = // Создание списка
+    let element = Console.ReadLine()    // Задания значений списка
+    if element = "" then    // Если введена пустота то создается пустой список
+        []
+    else
+        element :: createList() // Рекурсивный вызов
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите элементы вашего списка: "  // Функция для создание списка
+    let list = createList()
+    let result = System.String.Concat(Array.ofList(list))
+    printf "Нужная строка: %s" result
+    0
---
+++ Lab 2/Lab 2/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open Microsoft.FSharp.Core
+
+let rec createList () = // Создание списка
+    let element = Console.ReadLine()    // Задания значений списка
+    if element = "" then    // Если введена пустота то создается пустой список
+        []
+    else
+        element :: createList() // Рекурсивный вызов
+
+let addToList element list = element :: list  // Добавление элемента в лист
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите список: "
+    let list = createList()
+    printf "%A" list
+    let temp = []
+    let result = null
+    for i in 0..List.length list-1 do
+            let it = list[i]
+            let last = it.ToString().[it.ToString().Length-1]
+            let num = Int64.Parse(last.ToString())
+            let result = num :: temp
+            printfn " "
+    printfn "Искомый список: %A" result
+    0
