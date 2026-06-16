9aba51cb5f6e6562ba1184c9a2f37862377d0a21
Kriperus <krasnyhaleksandr123@gmail.com>
2026-06-17 01:16:03 +0500

    Added task 1
    Added key files for task 1

---
+++ Lab_2 task 1/Lab_2 task 1/Program.fs
@@ -0,0 +1,45 @@
+﻿open System
+
+// Получение последней цифры числа
+let getLastDigit (num: float) =
+    num.ToString()
+    |> Seq.filter Char.IsDigit
+    |> Seq.last
+    |> string
+    |> int
+
+// Хвостовая рекурсия для ввода чисел
+let rec readNumbersTailRec acc =
+    printf "Введите число (или 'exit' для завершения): "
+    match Console.ReadLine() with
+    | input when input.ToLower() = "exit" -> 
+        List.rev acc
+    | input ->
+        match Double.TryParse(input) with
+        | true, num ->
+            printfn "Добавлено: %.2f" num
+            readNumbersTailRec (num :: acc)
+        | false, _ ->
+            printfn "Ошибка ввода! Попробуйте снова"
+            readNumbersTailRec acc
+
+[<EntryPoint>]
+let main argv =
+    printfn "Вводите вещественные числа по одному"
+    printfn "Для завершения введите 'exit'\n"
+    
+    let numbers = readNumbersTailRec []
+    
+    match numbers with
+    | [] -> 
+        printfn "\nНе введено ни одного числа!"
+    | _ ->
+        printfn "\nИсходный список: %A" numbers
+        
+        // List.map
+        let lastDigits = 
+            numbers 
+            |> List.map getLastDigit
+        
+        printfn "Последние цифры: %A" lastDigits
+    0
